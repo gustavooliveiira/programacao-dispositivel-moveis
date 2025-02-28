cff1eb9ecccdb32b4e09003b8dcf132e716b7f47
Gustavooliveira <gustavo.lbsg2017@gmail.com>
2025-02-27 21:43:03 -0300

    codigo da aula02

---
+++ aulas/aula02/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Text, Button, StyleSheet } from "react-native";
+
+function Titulo(props){
+  return <Text>{props.children}</Text>; //titulo
+}
+
+function App(){
+  const [estado, setEstado] = useState(0);
+  return (
+    <>
+      <Titulo>Meu App</Titulo>;
+      <Text style= { styles.titulo}>Olá! Pessoal</Text>; 
+      <Text style= {styles.titulo}>{estado}</Text>;
+      <Button title="Clique aqui" onPress={() => setEstado(estado + 1)}/>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  titulo : {
+    fontSize : 24,
+    padding : 24
+  }
+});
+export default App;
