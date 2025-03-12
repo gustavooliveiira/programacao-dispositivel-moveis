359bfddc26971a4c1e231de3ccc0f50ec06cb05f
Gustavooliveira <gustavo.lbsg2017@gmail.com>
2025-03-11 21:51:04 -0300

    aula 03, adicionando screens

--- aulas/aula03/App.js
+++ aulas/aula03/App.js
@@ -1,18 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import Flexbox from "./screens/Flexbox";
+import GaleriaScreen from "./screens/GaleriaScreen";
+import PadraoAndroid from "./screens/PadraoAndroid";
+import PadraoIOS from "./screens/PadraoIOS"
 
 function App () {
-  return <View style={{flex:1, flexDirection: "row",backgroundColor: "white"}}>
-  <View style={{flex:1, 
-                justifyContent: "space-evenly", 
-                backgroundColor: "red",
-                alignItems :"stretch"
-                }}>
-    <View style={{ height:50,  backgroundColor: "black" }}></View>
-    <View style={{ height:50,  backgroundColor: "blue" }}></View>
-    <View style={{ height:50,  backgroundColor: "yellow" }}></View>
-  </View>
-    <View style={{flex:1, backgroundColor: "green"}}></View>
-  </View>;
+  return (
+    <GaleriaScreen />
+  );
 }
 
 
